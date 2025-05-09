61521d074d1093c5acb7e5ae7d3cc76420b6210f
Sergio-Redraw <sergiomatosmoreiraa@gmail.com>
2025-05-09 16:18:14 -0300

    Refine .gitignore e remove arquivos rastreados indevidamente

--- build/install_app_manager.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -euxo pipefail
-git clone https://github.com/ashleykleynhans/app-manager.git /app-manager
-cd /app-manager
-git checkout tags/${APP_MANAGER_VERSION}
-npm install
